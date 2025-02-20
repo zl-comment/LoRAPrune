941e89c736bf3f8498545b95b4b7706ce9a7343b
张磊 <62880914+zl-comment@users.noreply.github.com>
2025-02-20 10:55:27 +0800

    prune转化成一个整模型的函数命令

---
+++ script/convert_to_hf_windows.sh
@@ -0,0 +1,4 @@
+python convert_to_hf.py --output_dir ./output --target_save_path ./output_hf --base_model_name_or_path "D:/ZLCODE/model/Llama-2-7b-chat-hf" --checkpoint_dir "checkpoint-12"
+
+
+python convert_to_hf.py --output_dir ./output-v2 --target_save_path ./output_hf-v2 --base_model_name_or_path "D:/ZLCODE/model/Llama-2-7b-chat-hf" --checkpoint_dir "checkpoint-20"
