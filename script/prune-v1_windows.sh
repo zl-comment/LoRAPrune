9f0aa6d7ec1ff0f6720b9f17eab9902cd62ccd98
张磊 <62880914+zl-comment@users.noreply.github.com>
2025-02-20 16:29:22 +0800

    修改了命令参数

--- script/prune-v1_windows.sh
+++ script/prune-v1_windows.sh
@@ -1,4 +1,4 @@
-python prune-v1.py --base_model "D:\ZLCODE\model\Llama-2-7b-chat-hf" --data_path "./data/harmful_behaviors.csv" --output_dir "./output" --batch_size 128 --micro_batch_size 4 --num_epochs 3 --learning_rate 3e-4  #0.01%的剪枝率 性能和原本基本一样对于我的数据集
+python prune-v1.py --base_model "D:\ZLCODE\model\Llama-2-7b-chat-hf" --data_path "./data/harmful_behaviors.csv" --output_dir "./output" --batch_size 128 --micro_batch_size 4 --num_epochs 20 --learning_rate 3e-4 --ratio 0.5 --prune_freq 5 --warmup_iters 0.05 --cooldown_iters 0.05 --prune_metric magnitude --lora_r 8 --lora_alpha 16 --lora_dropout 0.05
 
 python prune-v1.py --base_model "D:\ZLCODE\model\Llama-2-7b-chat-hf" --data_path "./data/harmful_behaviors.csv" --output_dir "./output-v2" --batch_size 128 --micro_batch_size 4 --num_epochs 5 --learning_rate 5e-4 --ratio 0.7 --prune_freq 5 --warmup_iters 0.05 --cooldown_iters 0.05 --prune_metric magnitude --lora_r 16 --lora_alpha 32 --lora_dropout 0.1
 
