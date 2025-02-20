81ac9e3ef69af454b69fc422672d556996272a49
张磊 <62880914+zl-comment@users.noreply.github.com>
2025-02-20 10:54:13 +0800

    评估prune的函数的命令

---
+++ script/inference-v1_windows.sh
@@ -0,0 +1,3 @@
+python inference-v1.py --base_model "D:\ZLCODE\model\Llama-2-7b-chat-hf" --lora_weights "./output/checkpoint-12"
+
+python inference-v1.py --base_model "D:\ZLCODE\model\Llama-2-7b-chat-hf" --lora_weights "./output-v2/checkpoint-20"
